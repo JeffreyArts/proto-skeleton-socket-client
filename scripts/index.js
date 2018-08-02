1dc7e3814a8dd8d3faa6a4ec8031ff275738c23b
JeffreyArts <sjeffff@gmail.com>
2018-08-02 16:56:52 +0200

    Add support for room.join, room.left & room.current events

--- scripts/index.js
+++ scripts/index.js
@@ -5,9 +5,29 @@ const socket = io('http://localhost:3001');
 
 socket.on('connect', onConnect);
 
+socket.on('room.joined', m => {
+    socket.emit('room.getUserlist')
+});
+socket.on('room.left', m => {
+    socket.emit('room.getUserlist')
+})
+
+socket.on('room.getUserlist.success', m => {
+    document.querySelector("#roomUsers").innerText = m.length;
+})
+
 function onConnect(){
-  console.log('connect ' + socket.id);
+    joinRoom(document.querySelector("#roomList").value);
 }
+
+const joinRoom = v => {
+    socket.emit("room.join", v);
+    socket.emit('room.getUserlist')
+    document.querySelector("#toEveryoneInRoomResult").innerText = "";
+    document.querySelector("#toOthersInRoomResult").innerText = "";
+
+}
+
 
 const blink = target => {
     target.style = "background-color: chartreuse;";
@@ -42,6 +62,10 @@ socket.on('toSelf.success', m => {
 })
 
 
+
+
+
+
 // To Everyone
 document.querySelector("#toEveryoneButton").onclick = () => {
     const m = document.querySelector("#toEveryoneMessage").value;
@@ -53,6 +77,28 @@ socket.on('toEveryone.success', m => {
     blink(document.querySelector("#toEveryoneSignalIn"))
     document.querySelector("#toEveryoneResult").innerText = m;
 })
+
+
+
+
+// To Everyone !! IN ROOM !!
+document.querySelector("#toEveryoneInRoomButton").onclick = () => {
+    const m = document.querySelector("#toEveryoneInRoomMessage").value;
+    blink(document.querySelector("#toEveryoneInRoomSignalOut"))
+    socket.emit('toEveryoneInRoom', m)
+}
+
+socket.on('toEveryoneInRoom.success', m => {
+    blink(document.querySelector("#toEveryoneInRoomSignalIn"))
+    document.querySelector("#toEveryoneInRoomResult").innerText = m;
+})
+
+
+
+
+
+
+
 
 // To Others
 document.querySelector("#toOthersButton").onclick = () => {
@@ -66,6 +112,25 @@ socket.on('toOthers.success', m => {
     document.querySelector("#toOthersResult").innerText = m;
 })
 
+
+
+
+// To Others !! IN ROOM !!
+document.querySelector("#toOthersInRoomButton").onclick = () => {
+    const m = document.querySelector("#toOthersInRoomMessage").value;
+    blink(document.querySelector("#toOthersInRoomSignalOut"))
+    socket.emit('toOthersInRoom', m)
+}
+
+socket.on('toOthersInRoom.success', m => {
+    blink(document.querySelector("#toOthersInRoomSignalIn"))
+    document.querySelector("#toOthersInRoomResult").innerText = m;
+})
+
+
+
+
+
 // Get userlist
 document.querySelector("#getUserlistButton").onclick = () => {
     // const m = document.querySelector("#getUserlistMessage").value;
@@ -77,3 +142,25 @@ socket.on('getUserlist.success', m => {
     blink(document.querySelector("#getUserlistSignalIn"))
     document.querySelector("#getUserlistResult").innerText = JSON.stringify(m);
 })
+
+
+
+
+
+// Get Current room
+document.querySelector("#getCurrentRoomButton").onclick = () => {
+    blink(document.querySelector("#getCurrentRoomSignalOut"))
+    socket.emit('room.current')
+}
+
+socket.on('room.current.success', m => {
+    blink(document.querySelector("#getCurrentRoomSignalIn"))
+    document.querySelector("#getCurrentRoomResult").innerText = JSON.stringify(m);
+})
+
+
+
+// Change room
+document.querySelector("#roomList").onchange = () => {
+    joinRoom(document.querySelector("#roomList").value);
+}
